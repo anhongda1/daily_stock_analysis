8d84f6a30d5b0d6d09dcfa7d0140103f90c94467
anhongda1 <945297590@qq.com>
2026-03-24 01:02:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -266,11 +266,11 @@ def run_full_analysis(
     这是定时任务调用的主函数
     """
     try:
-    if stock_codes is None:
+       if stock_codes is None:
         config.refresh_stock_list()
-    stock_list = [str(item) for item in ["601868", "601669"]]
-    effective_codes = stock_codes if stock_codes is not None else stock_list
-    filtered_codes, effective_region, should_skip = _compute_trading_day_filter(config, args, effective_codes)
+        stock_list = [str(item) for item in ["601868", "601669"]]
+        effective_codes = stock_codes if stock_codes is not None else stock_list
+        filtered_codes, effective_region, should_skip = _compute_trading_day_filter(config, args, effective_codes)
         if should_skip:
             logger.info(
                 "今日所有相关市场均为非交易日，跳过执行。可使用 --force-run 强制执行。"
